27d426543f1f644a3b4c323b56af2bf4d023cd0e
abhishekoko <abhishek34149@gmail.com>
2026-03-11 19:09:13 +0530

    Add database

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,6 @@
+const convertBlog = (req, res) => {
+  // Placeholder logic for blog conversion
+  res.json({ message: "Blog conversion agent triggered successfully." });
+};
+
+module.exports = { convertBlog };
